65d007b03757febba0ab7cfe2389c528780aa5bc
ManyaS1011 <aynam1010@gmail.com>
2024-04-26 13:34:18 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,6 +6,29 @@ def smallest_missing_positive_integer(nums: List[int]) -> int:
 
     """
     pass
+function smallestMissingPositiveInteger(nums) {
+
+  // Implement the function smallest_missing_positive_integer
+
+  let n = nums.length;
+  let present = new Array(n + 1).fill(false);
+
+  for (let i = 0; i < n; i++) {
+      if (nums[i] > 0 && nums[i] <= n) {
+          present[nums[i]] = true;
+      }
+  }
+
+  for (let i = 1; i <= n; i++) {
+      if (!present[i]) {
+          return i;
+      }
+  }
+
+  return n + 1;
+}
+
+module.exports = smallestMissingPositiveInteger;
 
 
 
