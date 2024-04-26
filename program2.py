23ad4f6ad5a53fd48cd210303d70639b6862a79c
ManyaS1011 <aynam1010@gmail.com>
2024-04-26 13:49:58 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -10,24 +10,19 @@ def longest_substring(s: str) -> int:
     pass
 
 
-function longest_Substring(s) {
-    if (!s) return 0;
+def longest_substring(s):
+    if not s:
+        return 0
 
-    let charIndex = {};
-    let maxLength = 0;
-    let start = 0;
+    char_index = {}
+    max_length = 0
+    start = 0
 
-    for (let end = 0; end < s.length; end++) {
-        if (s[end] in charIndex && charIndex[s[end]] >= start) {
-            start = charIndex[s[end]] + 1;
-        } else {
-            maxLength = Math.max(maxLength, end - start + 1);
-        }
-        charIndex[s[end]] = end;
-    }
+    for end in range(len(s)):
+        if s[end] in char_index and char_index[s[end]] >= start:
+            start = char_index[s[end]] + 1
+        else:
+            max_length = max(max_length, end - start + 1)
+        char_index[s[end]] = end
 
-    return maxLength;
-}
-
-module.exports = { longest_Substring };
-
+    return max_length
